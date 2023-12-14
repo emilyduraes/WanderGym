0f4375bb284fecb9f174a79a81b84ec3873fa88f
emilyduraes <emilyduraes@yahoo.com.br>
2023-12-14 23:46:52 +0000

    adding liquibase plugin configurations and deleting maven wrapper

--- src/main/java/ie/wandergym/WanderGymApplication.java
+++ src/main/java/ie/wandergym/WanderGymApplication.java
@@ -2,8 +2,9 @@ package ie.wandergym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class WanderGymApplication {
 
 	public static void main(String[] args) {
