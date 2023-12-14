d5fd6726e303f98cefe0a3d693b6a42a2820d738
emilyduraes <emilyduraes@yahoo.com.br>
2023-12-14 14:16:11 +0000

    add project springboot structure

---
+++ src/main/java/ie/wandergym/WanderGymApplication.java
@@ -0,0 +1,13 @@
+package ie.wandergym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/wandergym/WanderGymApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.wandergym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
