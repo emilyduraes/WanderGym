4855503224da5ff8e063cba165e8f9b56b9f4b08
emilyduraes <emilyduraes@yahoo.com.br>
2023-12-30 14:52:58 +0000

    add cors configuration

--- src/main/java/ie/wandergym/WanderGymApplication.java
+++ src/main/java/ie/wandergym/WanderGymApplication.java
@@ -2,6 +2,9 @@ package ie.wandergym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WanderGymApplication {
@@ -10,4 +13,17 @@ public class WanderGymApplication {
 		SpringApplication.run(WanderGymApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				WebMvcConfigurer.super.addCorsMappings(registry);
+				registry
+						.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH");
+			}
+		};
+	}
 }
