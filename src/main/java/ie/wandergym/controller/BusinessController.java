3deeb4130528504c3095ca577f7567613620c751
emilyduraes <emilyduraes@yahoo.com.br>
2024-01-06 01:57:52 +0000

    change BusinessController authorization rules

--- src/main/java/ie/wandergym/controller/BusinessController.java
+++ src/main/java/ie/wandergym/controller/BusinessController.java
@@ -16,7 +16,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController()
 @RequestMapping("/business")
-@PreAuthorize("hasAnyRole('BUSINESS', 'SYSADMIN')")
 public class BusinessController {
     private final BusinessService service;
 
@@ -28,42 +27,49 @@ public class BusinessController {
     @ApiOperation(value = "Insert a new business into the database")
     @PostMapping(path = "/signin", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyRole('BUSINESS', 'SYSADMIN')")
     public void create(@RequestBody @Valid BusinessRequest request) {
         service.create(request);
     }
 
     @ApiOperation(value = "Find a business partner by ID from the database")
     @GetMapping(path = "/id/{id}")
+    @PreAuthorize("hasAnyRole('BUSINESS', 'SYSADMIN', 'USER')")
     public BusinessResponse get(@PathVariable Long id){
         return new BusinessResponse(service.find(id));
     }
 
     @ApiOperation(value = "Recover a fitness partner by name")
     @GetMapping(path = "/name/{name}")
+    @PreAuthorize("hasAnyRole('BUSINESS', 'SYSADMIN', 'USER')")
     public BusinessListResponse getByName(@PathVariable String name){
         return new BusinessListResponse(service.findByName(name));
     }
 
     @ApiOperation(value = "Recover all available fitness partners")
     @GetMapping(path = "/all")
+    @PreAuthorize("hasAnyRole('BUSINESS', 'SYSADMIN', 'USER')")
     public BusinessListResponse getAllBusinesses(){
         return new BusinessListResponse(service.findAllBusinesses());
     }
 
     @ApiOperation(value = "Delete a business partner from the database by ID")
     @DeleteMapping(path = "/id/{id}")
+    @PreAuthorize("hasAnyRole('BUSINESS', 'SYSADMIN')")
     public void delete(@PathVariable Long id){
         service.delete(id);
     }
 
     @ApiOperation(value = "Update a business partner from the database by ID")
     @PutMapping(path = "/id/{id}")
+    @PreAuthorize("hasAnyRole('BUSINESS', 'SYSADMIN')")
     public void update(@PathVariable Long id, @RequestBody @Valid BusinessRequest request){
         service.update(id, request);
     }
 
     @ApiOperation(value = "Deactivate a business partner account in the database")
     @PatchMapping(path = "/id/{id}/deactivate")
+    @PreAuthorize("hasAnyRole('BUSINESS', 'SYSADMIN')")
     public void deactivate(@PathVariable Long id){
         service.deactivate(id);
     }
