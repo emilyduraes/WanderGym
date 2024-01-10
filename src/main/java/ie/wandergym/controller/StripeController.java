39a598db2d38497184ee89a122aa7cec60bff694
emilyduraes <emilyduraes@yahoo.com.br>
2024-01-10 02:34:54 +0000

    add stripe integration

---
+++ src/main/java/ie/wandergym/controller/StripeController.java
@@ -0,0 +1,61 @@
+package ie.wandergym.controller;
+
+import com.google.gson.Gson;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import ie.wandergym.domain.entity.CheckoutPayment;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api")
+@CrossOrigin(origins = "http://localhost:4200")
+@PreAuthorize("hasAnyRole('SYSADMIN', 'USER')")
+public class StripeController {
+    private static void init(){
+        Stripe.apiKey = "sk_test_51OQdjgB4f2ZoQ9Fd6Zc2cusxuuqkXXAQu3PNIFflJ3wvBTg6S9RR0gF6eSDSVWlSuiML3UxDFjPPouAilsG33gtw00EwnJ2cNG";
+    }
+
+    // create a Gson object
+    private static Gson gson = new Gson();
+
+    @PostMapping("/payment")
+    /**
+     * Payment with Stripe checkout page
+     *
+     * @throws StripeException
+     */
+    public String paymentWithCheckoutPage(@RequestBody CheckoutPayment payment) throws StripeException {
+        // We initilize stripe object with the api key
+        init();
+        // We create a  stripe session parameters
+        SessionCreateParams params = SessionCreateParams.builder()
+                // We will use the credit card payment method
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+                .setMode(SessionCreateParams.Mode.PAYMENT).setSuccessUrl(payment.getSuccessUrl())
+                .setCancelUrl(
+                        payment.getCancelUrl())
+                .addLineItem(
+                        SessionCreateParams.LineItem.builder().setQuantity(payment.getQuantity())
+                                .setPriceData(
+                                        SessionCreateParams.LineItem.PriceData.builder()
+                                                .setCurrency(payment.getCurrency()).setUnitAmount(payment.getAmount())
+                                                .setProductData(SessionCreateParams.LineItem.PriceData.ProductData
+                                                        .builder().setName(payment.getName()).build())
+                                                .build())
+                                .build())
+                .build();
+        // create a stripe session
+        Session session = Session.create(params);
+        Map<String, String> responseData = new HashMap<>();
+        // We get the sessionId and we putted inside the response data you can get more info from the session object
+        responseData.put("id", session.getId());
+        // We can return only the sessionId as a String
+        return gson.toJson(responseData);
+    }
+}
